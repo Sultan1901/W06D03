3f19cf6af7af05bcfc2ccc4094cedbcbbf95f66c
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-16 21:30:10 +0300

    create server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+app.use(express.json())
+
+
+
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`server on ${PORT}`);
+})
