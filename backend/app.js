40163ed0bfb2e5cd6a0c1937bc851ee5ab62e288
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-16 22:29:10 +0300

    add put function

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,42 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
-const fs = require('fs')
-app.use(express.json())
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const fs = require("fs");
+app.use(express.json());
 
+let bro = [];
+fs.readFile("./bro.json", (err, data) => {
+  bro = JSON.parse(data.toString());
+});
 
-let bro = []
-fs.readFile('./bro.json',(err , data)=>{
-    bro = JSON.parse(data.toString())
-})
+app.get("/list", (req, res) => {
+  res.status(200);
+  res.json(bro);
+});
+app.post("/add", (req, res) => {
+  const { id, name, isFav, isDel } = req.body;
+  bro.push({ id: bro.length, name: name, isFav: isFav, isDel: isDel });
+  fs.writeFile("./bro.json", JSON.stringify(bro), (err, data) => {});
+  res.json(bro);
+  bro;
+});
+app.put("/update/:id", (req, res) => {
+  const bro1=bro.map((e) => {
+    if (e.id == req.params.id) {
+      return {
+        id: e.id,
+        name: req.body.name,
+        isFav: req.body.isFav,
+        isDel: req.body.isDel,
+      };
+    } else return e;
+  });
+  fs.writeFile("./bro.json", JSON.stringify(bro1), (err, data) => {
+    res.json(bro);
+    bro;
+  });
+});
 
-app.get('/list', (req , res)=>{
-    res.status(200);
-    res.json(bro)
-})
-app.post('/add' , (req , res)=>{
-    const {id , name ,isFav , isDel} = req.body
-    bro.push({id:bro.length , name:name ,isFav:isFav, isDel:isDel  })
-    fs.writeFile('./bro.json', JSON.stringify(bro),(err,data)=>{})
-    res.json(bro);bro
-})
-
-app.listen(PORT , ()=>{
-    console.log(`server on ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
+});
